972d1d8b017d174f1fc87e76b9180f066514ba3d
Jeremie Bourque <jeremie.bourque@usherbrooke.ca>
2020-09-26 23:13:10 -0400

    UDP with parameters

--- msg_to_antenna/src/gps_to_antenna.py
+++ msg_to_antenna/src/gps_to_antenna.py
@@ -2,24 +2,44 @@
 
 import rospy
 import socket
+import struct
+import math
 from sensor_msgs.msg import NavSatFix
 
 class GPSToAntenna: 
-    HOST = '10.42.0.30'  # server hostname
-    PORT = 65432
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     def __init__(self):
-        sub = rospy.Subscriber("fix", NavSatFix, self.fixCB)
-        self.s.connect((self.HOST, self.PORT))  # TODO: Verify that we can connect before attempting
+        self.host = rospy.get_param("host", "10.42.0.30")  # server hostname
+        self.port = rospy.get_param("port", 65432)  # server port
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP socket
 
-    def fixCB(self, msg):
-        coord = "%f;%f" % (msg.latitude, msg.longitude)
-        print(coord)
-        self.s.send(coord)
+        self.navsat_fix_format = "2f"  # lat, lon
+        self.sub = rospy.Subscriber("fix", NavSatFix, self.navsatFixCB)
+        self.time_between_msgs = rospy.get_param("time_between_msgs", 1)
 
-    def closeConnection(self):
-        self.s.close()  # TODO: When should the connection be closed?
+        self.lat = 999  # init to 999 for invalid value
+        self.lon = 999  # init to 999 for invalid value
+
+        print("Using host %s with port %d" % (self.host, self.port))
+        self.loop()
+
+    def loop(self):
+        print("gps_to_antenna node ready")
+        while not rospy.is_shutdown():
+            print("Sending: lat=%f  lon=%f" % (self.lat, self.lon))
+            msg = struct.pack(self.navsat_fix_format, self.lat, self.lon)
+            self.s.sendto(msg, (self.host, self.port))
+            rospy.sleep(self.time_between_msgs)
+
+    def navsatFixCB(self, msg):
+        if not math.isnan(msg.latitude):
+            self.lat = msg.latitude
+        else:
+            self.lat = 999
+        if not math.isnan(msg.longitude):
+            self.lon = msg.longitude
+        else:
+            self.lon = 999
+        print("Subscriber: lat=%f  lon=%f" % (msg.latitude, msg.longitude))
 
 
 if __name__ == "__main__":
