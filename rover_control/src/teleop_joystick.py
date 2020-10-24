5a66915257988343d6d41af69c1ae6ade4e6d0e0
Vincent Pelletier <vincentpelletier97@gmail.com>
2020-10-24 13:20:59 -0400

    Joystick callback function was made. Can receive from /joy and publish a twist

--- rover_control/src/teleop_joystick.py
+++ rover_control/src/teleop_joystick.py
@@ -6,13 +6,17 @@ from geometry_msgs.msg import Twist
 
 class TeleopJoystick():
     def __init__(self):
-        rospy.init_node("teleop_joystick")
+        rospy.init_node("teleop_joystick", anonymous = True)
         self.joy_sub = rospy.Subscriber('/joy', Joy, self.joyCB)
-        self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+        self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=2)
 
     def joyCB(self, data):
-        # TODO: your code here, remove pass
-        pass
+        twist = Twist()
+        twist.linear.x = data.axes[1]
+        twist.linear.y = data.axes[0]
+
+        rospy.loginfo("twist.linear.x: %f ; twist.linear.y %f", twist.linear.x, twist.linear.y)
+        self.cmd_pub.publish(twist)
 
 if __name__ == '__main__':
     teleop_joystick = TeleopJoystick()
