7c75f8ae0c6a98685370442d83a53d2043d99e83
Jeremie Bourque <jeremie.bourque@usherbrooke.ca>
2020-10-25 13:13:24 -0400

    fixed parameter errors

--- rover_control/src/teleop_joystick.py
+++ rover_control/src/teleop_joystick.py
@@ -5,41 +5,34 @@ from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
 
 class TeleopJoystick():
-    def __init__(self):
-        rospy.init_node("teleop_joystick", anonymous = True)
-        self.joy_sub = rospy.Subscriber('/joy', Joy, self.joyCB)
-        self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=2)
+   def __init__(self):
+      rospy.init_node("teleop_joystick", anonymous = True)
+      self.joy_sub = rospy.Subscriber('/joy', Joy, self.joyCB)
+      self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=2)
 
-        #Getting parameters initialized at the launch file
-        if rospy.has_param("joy/enable_button"):
-           self.enable_button = rospy.get_param("joy/enable_button")
-        if rospy.has_param("joy/enable_turbo_button"):
-           self.enable_turbo_button = rospy.get_param("joy/enable_turbo_button")
-        if rospy.has_param("joy/scale_linear"):
-           self.linear_scaling = rospy.get_param("joy/scale_linear")
-        if rospy.has_param("joy/scale_linear_turbo"):
-           self.linear_scaling_turbo = rospy.get_param("joy/scale_linear_turbo")
-        if rospy.has_param("joy/scale_angular"):
-           self.angular_scaling = rospy.get_param("joy/scale_angular")
-        if rospy.has_param("joy/axis_linear"):
-           self.axis_linear = rospy.get_param("joy/axis_linear")
-        if rospy.has_param("joy/axis_angular"):
-           self.axis_angular = rospy.get_param("joy/axis_angular")
+      #Getting parameters initialized at the launch file
+      self.enable_button = rospy.get_param("~enable_button", 4)
+      self.enable_turbo_button = rospy.get_param("~enable_turbo_button", 5)
+      self.linear_scaling = rospy.get_param("~scale_linear", 1.0)
+      self.linear_scaling_turbo = rospy.get_param("~scale_linear_turbo", 4.0)
+      self.angular_scaling = rospy.get_param("~scale_angular", 1.0)
+      self.axis_linear = rospy.get_param("~axis_linear", 1)
+      self.axis_angular = rospy.get_param("~axis_angular", 0)
 
-    def joyCB(self, data):
-        twist = Twist()
-        twist.linear.x = data.axes[self.axis_linear]
-        twist.angular.z = data.axes[self.axis_angular]
+   def joyCB(self, data):
+      twist = Twist()
+      twist.linear.x = data.axes[self.axis_linear]
+      twist.angular.z = data.axes[self.axis_angular]
 
-        if data.buttons[self.enable_button] == 1: #Verify the dead man button is active (LB)
-            twist.angular.z *= self.angular_scaling #Both the turbo mode and normal mode share the same angular speed
-            
-            if data.buttons[self.enable_turbo_button] == 1:
-                twist.linear.x *= self.linear_scaling_turbo
-                self.cmd_pub.publish(twist)
-            else:
-                twist.linear.x *= self.linear_scaling 
-                self.cmd_pub.publish(twist)
+      if data.buttons[self.enable_button] == 1: #Verify the dead man button is active (LB)
+         twist.angular.z *= self.angular_scaling #Both the turbo mode and normal mode share the same angular speed
+         
+         if data.buttons[self.enable_turbo_button] == 1:
+               twist.linear.x *= self.linear_scaling_turbo
+               self.cmd_pub.publish(twist)
+         else:
+               twist.linear.x *= self.linear_scaling 
+               self.cmd_pub.publish(twist)
         
 
 if __name__ == '__main__':
