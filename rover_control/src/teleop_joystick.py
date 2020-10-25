fc4f8c4e1c67bc1358acf3b7bc2f820f8d26527c
Vincent Pelletier <vincentpelletier97@gmail.com>
2020-10-25 15:07:27 -0400

    Every scaling factor are beginning at 0. If the deadman is activated, then their value may change. Furthermore, when deadman is untoggled, parameter values are reset to 0.

--- rover_control/src/teleop_joystick.py
+++ rover_control/src/teleop_joystick.py
@@ -6,33 +6,46 @@ from geometry_msgs.msg import Twist
 
 class TeleopJoystick():
    def __init__(self):
-      rospy.init_node("teleop_joystick", anonymous = True)
-      self.joy_sub = rospy.Subscriber('/joy', Joy, self.joyCB)
-      self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=2)
+        rospy.init_node("teleop_joystick", anonymous = True)
+        self.joy_sub = rospy.Subscriber('/joy', Joy, self.joyCB)
+        self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=2)
 
-      #Getting parameters initialized at the launch file
-      self.enable_button = rospy.get_param("~enable_button", 4)
-      self.enable_turbo_button = rospy.get_param("~enable_turbo_button", 5)
-      self.linear_scaling = rospy.get_param("~scale_linear", 1.0)
-      self.linear_scaling_turbo = rospy.get_param("~scale_linear_turbo", 4.0)
-      self.angular_scaling = rospy.get_param("~scale_angular", 1.0)
-      self.axis_linear = rospy.get_param("~axis_linear", 1)
-      self.axis_angular = rospy.get_param("~axis_angular", 0)
+        #Getting parameters initialized at the launch file
+        self.enable_button = rospy.get_param("~enable_button", 4)
+        self.enable_turbo_button = rospy.get_param("~enable_turbo_button", 5)
+        self.linear_scaling = rospy.get_param("~scale_linear", 0.0)
+        self.linear_scaling_turbo = rospy.get_param("~scale_linear_turbo", 0.0)
+        self.angular_scaling = rospy.get_param("~scale_angular", 0.0)
+        self.axis_linear = rospy.get_param("~axis_linear", 1)
+        self.axis_angular = rospy.get_param("~axis_angular", 0)
 
    def joyCB(self, data):
-      twist = Twist()
-      twist.linear.x = data.axes[self.axis_linear]
-      twist.angular.z = data.axes[self.axis_angular]
+        twist = Twist()
+        twist.linear.x = data.axes[self.axis_linear]
+        twist.angular.z = data.axes[self.axis_angular]
 
-      if data.buttons[self.enable_button] == 1: #Verify the dead man button is active (LB)
-         twist.angular.z *= self.angular_scaling #Both the turbo mode and normal mode share the same angular speed
-         
-         if data.buttons[self.enable_turbo_button] == 1:
-               twist.linear.x *= self.linear_scaling_turbo
-               self.cmd_pub.publish(twist)
-         else:
-               twist.linear.x *= self.linear_scaling 
-               self.cmd_pub.publish(twist)
+        if data.buttons[self.enable_button] == 1: #Verify the dead man button is active (LB)
+            rospy.set_param("~scale_angular", 1.0)
+            self.angular_scaling = rospy.get_param("~scale_angular") # updates the scaling
+            twist.angular.z *= self.angular_scaling #Both the turbo mode and normal mode share the same angular speed
+
+            if data.buttons[self.enable_turbo_button] == 1:
+                rospy.set_param("~scale_linear_turbo", 4.0)
+                self.linear_scaling_turbo = rospy.get_param("~scale_linear_turbo")
+                twist.linear.x *= self.linear_scaling_turbo
+                self.cmd_pub.publish(twist)
+            else:
+                rospy.set_param("~scale_linear", 1.0)
+                self.linear_scaling = rospy.get_param("~scale_linear")
+                twist.linear.x *= self.linear_scaling 
+                self.cmd_pub.publish(twist)
+        else:
+            self.linear_scaling = rospy.set_param("~scale_linear", 0.0) #If no deadman is active, sets all param to 0 and twists are sent at 0
+            self.linear_scaling_turbo = rospy.set_param("~scale_linear_turbo", 0.0)
+            self.angular_scaling = rospy.get_param("~scale_angular", 0.0)
+            twist.angular.z = 0.0
+            twist.linear.x = 0.0
+            self.cmd_pub.publish(twist)
         
 
 if __name__ == '__main__':
