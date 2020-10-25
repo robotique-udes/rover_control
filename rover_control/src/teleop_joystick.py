c3e174ff4c03312c5b85ce86c3895020a2c7ea80
Plopao <42898536+Plopao@users.noreply.github.com>
2020-10-25 10:18:45 -0400

    Update teleop_joystick
    Changement des variables

--- rover_control/src/teleop_joystick.py
+++ rover_control/src/teleop_joystick.py
@@ -9,13 +9,15 @@ class TeleopJoystick():
         rospy.init_node("teleop_joystick", anonymous = True)
         self.joy_sub = rospy.Subscriber('/joy', Joy, self.joyCB)
         self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=2)
+        self.enable_button = rospy.get_param("~enable_button", 0)
+        self.enable_turbo_button = 0
 
     def joyCB(self, data):
         twist = Twist()
         twist.linear.x = data.axes[1]
-        twist.linear.y = data.axes[0]
+        twist.angular.z = data.axes[0]
 
-        rospy.loginfo("twist.linear.x: %f ; twist.linear.y %f", twist.linear.x, twist.linear.y)
+        rospy.loginfo("twist.linear.x: %f ; twist.linear.z %f", twist.linear.x, twist.linear.z)
         self.cmd_pub.publish(twist)
 
 if __name__ == '__main__':
